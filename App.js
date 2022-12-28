34eba513111d27704825ea98ab585c580fec69cf
yuriy.fedyuk <yfedi@softserveinc.com>
2022-12-28 16:14:26 +0200

    Useless change

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const express = require("express");
+
 function App() {
   const [data, setData] = React.useState(null);
 
